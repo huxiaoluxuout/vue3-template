679496ad1e05d65b4e79cc7d7e2b2146297e93ec
辶开呀 <914044753@qq.com>
2024-12-21 17:35:53 +0800

    旋转轮盘样式

--- components/ylx-components/ylx-test-animation-image.vue
+++ components/ylx-components/ylx-test-animation-image.vue
@@ -1,39 +1,45 @@
 <template>
 
-  <view class="box" :style="{transform:`rotate(${degDiff}deg)`}" @touchmove="touchmove" @touchstart="touchStart" @touchend="touchEnd">
-
-    <image class="image-item" :class="`${useRotateBack?'image-item-rotateback':'image-item-rotate'}`" v-for="(item,index) in imageGroups" :key="index"
-           :src="item"
+  <view id="box" class="box" :style="{ transform:`rotate(${degDiff}deg)`, width: `${width}`, height: `${width}`}"
+        @touchmove.stop="touchmove" @touchstart="touchStart" @touchend="touchEnd">
+    <image class="image-item" :class="imageItemTransform"
+           v-for="(itemUrl,index) in imageGroups" :key="index" :src="itemUrl"
            :style="{
+             left:`${positionOffset}`,top:`${positionOffset}`, width: `${itemImageWidth}`, height: `${itemImageWidth}`,
              transform:`rotate(${index * degItem}deg) translate(${offset}) rotate(${(-1 * index * degItem) - degDiff}deg)`,
              '--end-transform':`rotate(${index * degItem}deg) translate(${offset}) rotate(${(-1 * index * degItem) - degDiff}deg)`,
         }"
+           @click.stop="itemClick(index)"
     >
     </image>
 
-    <image :style="{transform:`translate(-50%,-50%) rotate(${-1 * degDiff}deg)`}" @click="setToggleAnimation" class="center" src="/static/love/1.png"></image>
+    <image id="center"
+           :style="{
+             transform:`translate(-50%,-50%) rotate(${-1 * degDiff}deg)`,
+               width: `${centerWidth}`,
+               height: `${centerWidth}`
+           }" @click="setToggleAnimation" class="center" :src="centerImageSrc">
+    </image>
 
   </view>
-
-  <!--  <view class="" style="position: fixed;top: 50%;left: 0;right: 0;display: flex">
-      <button @click="jia">++</button>
-      <button @click="jian">&#45;&#45;</button>
-      <button @click="rest">0</button>
-      <button @click="setToggleAnimation">animation</button>
-    </view>-->
-
 
 </template>
 
 <script>
-import YlxImage from "@/components/ylx-components/ylx-image.vue";
+
+
+let lastValue = 0
 
 export default {
-  components: {YlxImage},
-  data() {
-    return {
-      imageGroups: [
-        // "/static/love/1.png",
+  props: {
+    width: {
+      type: String,
+      default: '500rpx'
+    },
+    imageGroups: {
+      type: Array,
+      default: () => ([
+        "/static/love/1.png",
         "/static/love/2.png",
         "/static/love/3.png",
         "/static/love/4.png",
@@ -42,32 +48,39 @@ export default {
         "/static/love/7.png",
         "/static/love/8.png",
         "/static/love/9.png",
+        "/static/love/10.png",
+        "/static/love/11.png",
+        "/static/love/12.png",
+      ])
+    },
+    itemImageWidth: {
+      type: String,
+      default: '90rpx'
+    },
 
-        // "/static/love/10.png",
-        // "/static/love/11.png",
-        /*       "/static/love/12.png",
-               "/static/love/13.png",
-               "/static/love/14.png",
-               "/static/love/15.png",
-               "/static/love/16.png",
-               "/static/love/17.png",
-               "/static/love/18.png",*/
-      ],
+    centerWidth: {
+      type: String,
+      default: '60rpx'
+    },
+    centerImageSrc: {
+      type: String,
+      default: '/static/love/1.png'
+    },
 
+    // 控制灵敏度
+    rotateStep: {
+      type: Number,
+      default: 5
+    },
+    isOpen: Boolean
+  },
+  data() {
+    return {
       degSteps: 0,
-      startX: 0,
-      startY: 0,
-      lastX: 0,
-      lastY: 0,
-      lastSpeedX: 0,  // 新增：记录上一次的 X 方向速度
-      lastSpeedY: 0,  // 新增：记录上一次的 Y 方向速度
-      startTime: 0,
-      // 阻尼
-      damping: 0.3,
-      useRotateBack: false,
-
-      offset: '130%'
-      // offset: '100rpx'
+      useRotateBack: !this.isOpen,
+      centerPointX: 0,
+      centerPointY: 0,
+      radius: '100rpx',//半径
     };
   },
   computed: {
@@ -81,66 +94,88 @@ export default {
     degDiff() {
       return this.degSteps * (this.degItem / 100)
     },
+    offset() {
+      return `calc(${this.radius} + calc(${this.itemImageWidth} / 20))`
+    },
+    positionOffset() {
+      return `calc(${this.radius} - calc(${this.itemImageWidth} / 2))`
+    },
+    imageItemTransform() {
+      return `${this.useRotateBack ? 'image-item-rotateback' : 'image-item-rotate'}`
+    },
 
   },
   mounted() {
-
+    const query = uni.createSelectorQuery().in(this);
+    query
+        .select("#box")
+        .boundingClientRect((data) => {
+          this.radius = Math.floor((data.width / 2)) + 'px'
+          this.centerPointX = Math.floor(data.left + (data.width / 2))
+          this.centerPointY = Math.floor(data.top + (data.height / 2))
+        })
+        .exec();
   },
 
   methods: {
-
+    itemClick(index) {
+      this.$emit('itemClick', index)
+    },
     setToggleAnimation() {
+      while (this.degSteps < 0) {
+        this.degSteps += 1
+      }
+      while (this.degSteps > 0) {
+        this.degSteps -= 1
+      }
+      this.$emit('centerClick', this.useRotateBack)
       this.useRotateBack = !this.useRotateBack
-      this.degSteps = 0
-
     },
 
     touchStart(event) {
-      this.startX = event.changedTouches[0].pageX;
-      this.startY = event.changedTouches[0].pageY;
-      this.startTime = new Date().getTime();
-    },
-    touchmove(event) {
-      const currentX = event.changedTouches[0].pageX;
-      const currentY = event.changedTouches[0].pageY;
-
-      const distanceX = currentX - this.startX;
-      const distanceY = currentY - this.startY;
-
-      const duration = new Date().getTime() - this.startTime;
-
-      const speedX = distanceX / duration;
-      const speedY = distanceY / duration;
-
-      let sin = speedY / speedX
-
-      if (sin > 0) {
-        if (Math.abs(sin) > this.damping) {
-          console.log('向右下===》')
-          this.degSteps += 2
-        }
-      } else {
-        if (Math.abs(sin) > this.damping) {
-          console.log('《===向左上')
-          this.degSteps -= 2
-
-        }
-      }
+      lastValue = 0
     },
     touchEnd(event) {
-
+      lastValue = 0
     },
 
-    jia() {
-      this.degSteps += 20
-    },
-    jian() {
-      this.degSteps -= 20
+    touchmove(event) {
+      if (this.useRotateBack) return;
+      const currentX = event.changedTouches[0].pageX;
+      const currentY = event.changedTouches[0].pageY;
+      const distanceX = Math.floor(currentX - this.centerPointX);
+      const distanceY = Math.floor(currentY - this.centerPointY);
+      if (distanceX === 0) return
+      if (distanceY === 0) return
 
-    },
-    rest() {
-      this.degSteps = 0
+      let currentValue = (distanceX / distanceY)
+      let currentChange = currentValue - lastValue > 0
 
+     /* if (distanceX > 0 && distanceY > 0) {
+        this.setDegSteps(currentChange)
+      } else if (distanceX < 0 && distanceY < 0) {
+        this.setDegSteps(currentChange)
+
+      } else if (distanceX < 0 && distanceY > 0) {
+        this.setDegSteps(currentChange)
+
+      } else if (distanceX > 0 && distanceY < 0) {
+        this.setDegSteps(currentChange)
+      }*/
+      this.setDegSteps(currentChange)
+
+
+      lastValue = currentValue
+    },
+
+    setDegSteps(currentChange) {
+      if (!currentChange) {
+        // 逆时针转动
+        this.degSteps += this.rotateStep
+      } else {
+        // 顺时针转动
+        this.degSteps -= this.rotateStep
+      }
     },
 
   }
@@ -148,40 +183,31 @@ export default {
 </script>
 
 <style scoped>
-
 .box {
   box-sizing: border-box;
-  width: 200rpx;
-  height: 200rpx;
   position: relative;
   transition: transform 100ms;
   border-radius: 50%;
-  border: 1px solid #000;
 
 }
 
 .image-item {
-  width: 80rpx;
-  height: 80rpx;
   position: absolute;
-  left: 25%;
-  top: 25%;
-  //transform-origin: 200rpx 200rpx;
 }
 
 .image-item-rotate {
-  animation: rotate 1s 1 forwards;
+  animation: rotate 600ms 1 forwards;
 
 }
 
 .image-item-rotateback {
-  animation: rotateback 1s 1 forwards;
+  animation: rotateback 600ms 1 forwards;
 
 }
 
 @keyframes rotate {
   0% {
-    transform: rotate(-135deg) translate(0) rotate(-135deg) scale(1);
+    transform: rotate(-270deg) translate(0) rotate(-270deg) scale(1);
   }
   100% {
     transform: var(--end-transform);
@@ -193,18 +219,15 @@ export default {
     transform: var(--end-transform);
   }
   100% {
-    transform: rotate(-135deg) translate(0) rotate(-135deg) scale(0);
+    transform: rotate(-270deg) translate(0) rotate(-270deg) scale(0);
   }
 }
 
 
 .center {
   position: absolute;
-  width: 60rpx;
-  height: 60rpx;
   top: 50%;
   left: 50%;
-//animation: rotate-center 6s 1 forwards;
 
 }
 
--- testPage.vue
+++ testPage.vue
@@ -1,13 +1,31 @@
 <template>
   <view>
 
-    <view style="position:fixed;bottom: 400px;left: 0;right: 0;display:flex;justify-content: center;transform: translateY(50%)">
-      <ylx-test-animation-image></ylx-test-animation-image>
+    <view style="position:fixed;bottom:420px;left: 0;right: 0;display:flex;justify-content: center;transform: translateY(50%);">
+      <ylx-test-animation-image width="500rpx" :rotate-step="5" @centerClick="centerClick" @itemClick="itemClick"></ylx-test-animation-image>
     </view>
   </view>
 </template>
 
-<script setup lang="ts">
+<script>
+export default {
+
+  methods: {
+    centerClick(isOpen) {
+      uni.showToast({
+        title: isOpen ? '打开' : '关闭',
+        icon: 'none'
+      });
+    },
+    itemClick(index) {
+      uni.showToast({
+        title: `点击了第${index + 1}个`,
+        icon: 'none'
+      });
+    }
+  }
+}
+
 
 </script>
 
