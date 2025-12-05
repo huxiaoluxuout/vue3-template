91349a1926562453df0c8f339058c45f809e63d8
辶开呀 <914044753@qq.com>
2025-12-05 14:51:41 +0800

    修复bug

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -3,7 +3,7 @@
     mime lorem*12
     <ylx-navbar title="我的" bg-color="#fff"></ylx-navbar>
     登录状态：{{ hasLogin2 }}
-<button @click="loginFn">登录</button>
+    <button @click="loginFn">登录</button>
     登录状态 hasLogin3：{{ hasLogin3 }}
 
 
@@ -14,13 +14,13 @@
 import {ref, computed, watch, reactive} from 'vue';
 
 import {onLoad, onTabItemTap, onReachBottom, onPullDownRefresh} from '@dcloudio/uni-app'
-import {ylxInterceptorCall} from "@/ylxuniCore/useylxuni";
+import {ylxIntercept} from "@/ylxuniCore/useylxuni";
 
-const hasLogin2 = ref(ylxInterceptorCall.getIntercept.login)
-const hasLogin3 = computed(() => ylxInterceptorCall.getIntercept.login)
+const hasLogin2 = ref(ylxIntercept.getIntercepts.login)
+const hasLogin3 = computed(() => ylxIntercept.getIntercepts.login)
 
 function loginFn() {
-  ylxInterceptorCall.setInterceptKey('login',!ylxInterceptorCall.getInterceptKey('login'))
+  ylxIntercept.setIntercept('login',!ylxIntercept.getIntercepts.login)
 }
 </script>
 
